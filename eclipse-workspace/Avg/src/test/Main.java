76aff8a4abefa4a172d615d4aa53c42efd99a859
ks0510 <kshaikh051999@gmail.com>
2023-04-30 16:49:24 +0530

    Added java program

---
+++ eclipse-workspace/Avg/src/test/Main.java
@@ -0,0 +1,22 @@
+package test;
+import java.util.*;
+
+public class Main {
+	static int k=29;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);  
+		System.out.println("Enter number");
+		int a=sc.nextInt();
+		int b=56;
+		double c=98.5935;
+		double avg;
+		avg=(a+b+c+k)/4;
+		System.out.println("Average of Three numbers is ");
+		System.out.print(avg);
+		
+
+	}
+
+}
